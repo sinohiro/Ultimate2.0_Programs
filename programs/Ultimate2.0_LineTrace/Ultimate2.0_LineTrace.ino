3f7c3e5c4566026a4324b75d26702adf76418136
sinohiro <sinohiro@momo-mail.com>
2020-10-02 19:47:14 +0900

    LineTrace Update

--- programs/Ultimate2.0_LineTrace/Ultimate2.0_LineTrace.ino
+++ programs/Ultimate2.0_LineTrace/Ultimate2.0_LineTrace.ino
@@ -13,9 +13,9 @@ unsigned long last_time;
 MeMegaPiDCMotor motor1(PORT1B);  
 MeMegaPiDCMotor motor2(PORT2B);
 MeMegaPiDCMotor motor3(PORT3B); 
-uint8_t motorSpeed = 185;
-uint8_t motorSpeed2 = 100;
-uint8_t motorSpeed3 = 100;
+uint8_t motorSpeed = 70;
+uint8_t motorSpeed2 = 70;
+uint8_t motorSpeed3 = 70;
 uint8_t motorStop = 0;
 //motor_setup_end
 
@@ -31,23 +31,23 @@ void loop(){
   switch(sensorState){
     case S1_IN_S2_IN:
       Serial.println("Sensor 1 and 2 are inside of black line");
-      motor1.run(motorStop);
-      motor2.run(motorStop);
+      motor1.run(motorSpeed);
+      motor2.run(motorSpeed2);
       break;
     case S1_IN_S2_OUT:
       Serial.println("Sensor 2 is outside of black line");
       motor1.run(motorSpeed);
-      motor2.run(motorStop);
+      motor2.run(-motorSpeed3);
       break;
     case S1_OUT_S2_IN:
       Serial.println("Sensor 1 is outside of black line");
       motor2.run(motorSpeed2);
-      motor1.run(motorStop);
+      motor1.run(-motorSpeed3);
       break;
     case S1_OUT_S2_OUT:
       Serial.println("Sensor 1 and 2 are outside of black line");
-      motor1.run(motorSpeed);
-      motor2.run(motorSpeed2);
+      motor1.run(-motorSpeed3);
+      motor2.run(-motorSpeed3);
       break; 
   }
   delay(200);
