22e80e4294c3f4e5c7e9eb313cf5207a8b1b74ed
sinohiro <sinohiro@momo-mail.com>
2020-10-07 23:32:44 +0900

    add concept program

--- programs/Ultimate2.0_LineTraceUltraSoundSensor/Ultimate2.0_LineTraceUltraSoundSensor.ino
+++ programs/Ultimate2.0_LineTraceUltraSoundSensor/Ultimate2.0_LineTraceUltraSoundSensor.ino
@@ -14,10 +14,14 @@ unsigned long last_time;
 MeMegaPiDCMotor motor1(PORT1B);  
 MeMegaPiDCMotor motor2(PORT2B);
 MeMegaPiDCMotor motor3(PORT3B); 
-uint8_t motorSpeed = 70;
+MeMegaPiDCMotor motor4(PORT4B);
+uint8_t motorSpeed = 100;
 uint8_t motorSpeed2 = 70;
+uint8_t motorHiSpeed = 130;
+uint8_t motorHiSpeed2 = 130;
 uint8_t motorSpeed3 = 70;
 uint8_t motorStop = 0;
+int WallCount = 0;
 //motor_setup_end
 
 void setup(){
@@ -25,6 +29,11 @@ void setup(){
   pinMode(NE1, INPUT);
   attachInterrupt(digitalPinToInterrupt(interruptPin), blink,RISING);
   Serial.begin(9600);
+  motor3.run(-motorSpeed3);
+  motor4.run(-motorSpeed3);
+  delay(4000);
+  motor3.run(motorStop);
+  motor4.run(motorStop);
 }
 
 void loop(){
@@ -35,9 +44,32 @@ void loop(){
   Serial.println(" cm");
   delay(100);
 
-  if (ultraSensor.distanceCm() < 5){
+  if (ultraSensor.distanceCm() < 8){
     motor1.run(motorStop);
     motor2.run(motorStop);
+    motor3.run(motorSpeed3);
+    delay(2100);
+    motor3.run(motorStop);
+    if (WallCount == 0){
+      motor4.run(motorSpeed3);
+      delay(4000);
+      WallCount++;
+    }
+    else{
+      motor4.run(-motorSpeed3);
+      delay(4000);
+      WallCount = 0;
+    }
+    motor4.run(motorStop);
+    motor3.run(-motorSpeed3);
+    delay(4000);
+    motor3.run(motorStop);
+    motor1.run(-motorSpeed);
+    motor2.run(-motorSpeed2);
+    delay(1500);
+    motor1.run(motorHiSpeed);
+    motor2.run(-motorHiSpeed2);
+    delay(4000);
   }
   else{
     //LineTrace
@@ -51,12 +83,12 @@ void loop(){
       case S1_IN_S2_OUT:
         Serial.println("Sensor 2 is outside of black line");
         motor1.run(motorSpeed);
-        motor2.run(-motorSpeed3);
+        motor2.run(-motorSpeed2);
         break;
       case S1_OUT_S2_IN:
         Serial.println("Sensor 1 is outside of black line");
         motor2.run(motorSpeed2);
-        motor1.run(-motorSpeed3);
+        motor1.run(-motorSpeed);
         break;
       case S1_OUT_S2_OUT:
         Serial.println("Sensor 1 and 2 are outside of black line");
